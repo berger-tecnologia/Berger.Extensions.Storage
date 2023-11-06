7c65eef5f26e8df7c589626a95e26cafacf8c4ab
Rafael Cronemberger <rafael@berger.com.br>
2023-11-06 12:14:03 -0300

    Abstract Factory Pattern Implementation

--- src/Berger.Extensions.Storage/Models/Asset.cs
+++ src/Berger.Extensions.Storage/Models/Asset.cs
@@ -2,21 +2,14 @@
 
 namespace Berger.Extensions.Storage
 {
-    public class Asset<T> : BaseEntityWrapper, IAsset<T>
+    public class Asset : BaseEntityWrapper
     {
-        #region Constructors
-        public Asset(T value)
-        {
-            FileExtension = value;
-        }
-        #endregion
-
         #region Properties
         public Guid ContainerID { get; set; }
         public IModule Module { get; set; }
         public IContainer Container { get; set; }
         public string AttributeID { get; set; } = string.Empty;
-        public T FileExtension { get; private set; }
+        public FileExtension FileExtension { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Path { get; set; } = string.Empty;
         #endregion
